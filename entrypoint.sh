21dff53fac05acc90dc5da2b851c62e8aa67f4cc
Don Patterson <d_j_p_3@djp3.net>
2022-06-16 12:55:21 -0700

    Get default wallet working for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #/mining.sh &
+/walletsetup.sh &
 exec bitcoind
---
+++ walletsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# wait a while for the bitcoin node to bootstrap
+sleep 10
+
+bitcoin-cli createwallet "blockpliance"
+
