79f3a6fe6579321055932f570231025ebda401ab
Don Patterson <d_j_p_3@djp3.net>
2022-06-22 11:59:26 -0700

    Removed auto wallet creation so that it could happen in code

--- walletsetup.sh
+++ walletsetup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "Running wallet setup"
 # wait a while for the bitcoin node to bootstrap
-sleep 10
+#sleep 10
 
-bitcoin-cli -named createwallet wallet_name="blockpliance" descriptors=false load_on_startup=true
+#bitcoin-cli -named createwallet wallet_name="blockpliance" descriptors=false load_on_startup=true
 
+# We are doing this in code now
